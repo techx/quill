15d481233f8ad84bab8b1debf22707d06b40b6cf
chriswongtv <chris@cogifire.com>
2017-10-27 22:02:38 -0700

    Fix email verification bug

--- app/client/src/routes.js
+++ app/client/src/routes.js
@@ -218,7 +218,7 @@ angular.module('reg')
           $rootScope.fadeOut = true;
 
           $timeout(function() {
-            $state.go(toState.name);
+            $state.go(toState.name, toParams);
           }, 100);
         }
 
