8d7ecca3924284be47ba0a24cf05b0d7a277a990
chriswongtv <chris@cogifire.com>
2017-10-27 23:31:20 -0700

    Update resend verification email alert

--- app/client/views/dashboard/dashboardCtrl.js
+++ app/client/views/dashboard/dashboardCtrl.js
@@ -69,7 +69,7 @@ angular.module('reg')
         AuthService
           .resendVerificationEmail()
           .then(function(){
-            sweetAlert('Your email has been sent.');
+            sweetAlert('Resend Verification Email', 'Your email has been sent.', 'success');
           });
       };
 
