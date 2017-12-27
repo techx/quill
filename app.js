18ea4564dacb9e8f04d79b644675b9ec6a8798fb
Steven Roberts <sarj21@vt.edu>
2017-12-27 18:43:59 -0500

    Didnt actually add prospectus in this change

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(methodOverride());
 
 app.use('/', express.static(__dirname + '/www'));
 app.use('/register', express.static(__dirname + '/app/client'));
-app.use('/prospectus', express.static(__dirname + '/files/vthacks-v-prospectus.pdf'));
 app.use('/release', express.static(__dirname + '/files/VTHacks Media Release Statement.pdf'));
 
 // Routers =====================================================================
