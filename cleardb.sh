13b450b99a0d5c8fa5bfc7cfcca571d3e6c2f15d
Steven Roberts <sarj21@vt.edu>
2018-01-09 23:03:54 -0500

    Removing server side shell scripts
    These shouldn't be in the repo and aren't even used now

--- cleardb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This is dangerous, lets be safe here
-  mongo admin --eval "db.dropDatabase()"
-  mongo test --eval "db.dropDatabase()"
-  mongo config --eval "db.dropDatabase()"
-  mongo local --eval "db.dropDatabase()"
--- dbstart.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mongod --dbpath /home/protected/www+quill/db --bind_ip 127.0.0.1 --nohttpinterface
