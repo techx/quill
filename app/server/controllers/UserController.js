f4298ab423a3a7a0803208244ea69a3d477b5431
chriswongtv <chris@cogifire.com>
2018-01-15 15:51:37 -0800

    Integrate automated waiver document status check

--- app/server/controllers/UserController.js
+++ app/server/controllers/UserController.js
@@ -3,6 +3,7 @@ var User = require('../models/User');
 var Settings = require('../models/Settings');
 var Mailer = require('../services/email');
 var Stats = require('../services/stats');
+var Waiver = require('../services/waiver');
 
 var validator = require('validator');
 var moment = require('moment');
@@ -732,6 +733,21 @@ UserController.sendAcceptanceEmailById = function(id, callback) {
        return callback(err, user);
    });
  };
+
+UserController.markWaiverAsSigned = function(email, callback) {
+  User.findOneAndUpdate({
+    'email': email,
+    'status.admitted': true
+  },{
+    $set: {
+      'lastUpdated': Date.now(),
+      'confirmation.signatureLiability': Date.now()
+    }
+  }, {
+    new: true
+  },
+  callback);
+};
 
 UserController.getStats = function(callback){
   return callback(null, Stats.getUserStats());
