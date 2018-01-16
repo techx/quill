38b34e450022e66e816e09cd262827552426fc0e
Dale Mitchell <mitchell.1367@osu.edu>
2018-01-15 22:10:47 -0500

    Change app.js and app/client/index.html to match our registration root directory
    Probably a cleaner way to do this but I'm no Node.js expert (although I'm enjoying every part of this because I'm learning so much!)

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 app.use(methodOverride());
 
-app.use(express.static(__dirname + '/app/client'));
+app.use('/register', express.static(__dirname + '/app/client'));
 
 // Routers =====================================================================
 
