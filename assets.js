68778bc7a24ea8925df5901e6a5062492bddaeb3
Shea Belsky <shb95@cornell.edu>
2018-07-14 21:33:50 -0700

    Duplicate

--- assets.js
+++ assets.js
@@ -3,6 +3,7 @@ module.exports = [
     "angular-ui-router/release/angular-ui-router.min.js",
     "jquery/dist/jquery.min.js",
     "moment/min/moment.min.js",
+    "semantic-ui/dist/semantic.min.css",
     "semantic-ui/dist/semantic.min.js",
     "showdown/dist/showdown.min.js",
     "sweetalert/dist/sweetalert.min.js",
