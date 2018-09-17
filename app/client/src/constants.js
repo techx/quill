8c12b2dd800093f9dcdddbaddb5d3a86e940616b
mhendrick98 <mhendric@bu.edu>
2018-09-17 19:23:24 -0400

    fix timezone

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -8,20 +8,20 @@ angular
       "You should have received an email asking you verify your email. Click the link in the email and you can start your application!",
     INCOMPLETE_TITLE: "You still need to complete your application!",
     INCOMPLETE:
-      "If you do not complete your application before the [APP_DEADLINE], you will not be considered for admission!",
+      "If you do not complete your application before the [APP_DEADLINE]), you will not be considered for admission!",
     SUBMITTED_TITLE: "Your application has been submitted!",
     SUBMITTED:
       "Feel free to edit it at any time. However, once registration is closed, you will not be able to edit it any further.\n Please make sure your information is accurate before registration is closed!",
     CLOSED_AND_INCOMPLETE_TITLE: "Unfortunately, registration has closed.",
     CLOSED_AND_INCOMPLETE:
       "Because you have not completed your profile in time, you will not be eligible for admission.",
-    ADMITTED_AND_CAN_CONFIRM_TITLE: "You must confirm by [CONFIRM_DEADLINE].",
+    ADMITTED_AND_CAN_CONFIRM_TITLE: "You must confirm by [CONFIRM_DEADLINE]).",
     ADMITTED_AND_CANNOT_CONFIRM_TITLE:
-      "Your confirmation deadline of [CONFIRM_DEADLINE] has passed.",
+      "Your confirmation deadline of [CONFIRM_DEADLINE]) has passed.",
     ADMITTED_AND_CANNOT_CONFIRM:
       "Although you were accepted, you did not complete your confirmation in time.\nUnfortunately, this means that you will not be able to attend the event, as we must begin to accept other applicants on the waitlist.\nWe hope to see you again next year!",
     CONFIRMED_NOT_PAST_TITLE:
-      "You can edit your confirmation information until [CONFIRM_DEADLINE]",
+      "You can edit your confirmation information until [CONFIRM_DEADLINE])",
     DECLINED:
       "We're sorry to hear that you won't be able to make it to BostonHacks 2018! :(\nMaybe next year! We hope you see you again soon."
   })
