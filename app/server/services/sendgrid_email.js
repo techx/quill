e3fd2fca776ba5d548b9270fa6009e2605109736
Krishen Bhan <krishenb@uci.edu>
2018-10-27 16:23:23 -0700

    Adding email service for SendGrid API (similar interface to previous email service module) (#164)
    * Added module (and env variables) for SendGrid API email service.
    
    * Changed previous env variables back to defaults
    
    * Revision 1 for PR 164
    
    * added script to test sending all SendGrid template emails
    
    * Revision 2 for PR 164
    
    * Added dot env for sengrid email test
    
    * Fixed syntax error for 164
    
    * added more specific logging
    
    * tested testSendGridTemplates.js with ENV variables
    
    * added back accidental gulp package removal
    
    * Check bounds of SendGrid response
    
    * Fixed curly braces formatting
    
    * Update app/server/services/sendgrid_email.js
    
    Co-Authored-By: kbuci <krishenb@uci.edu>
    
    * Update app/server/services/sendgrid_email.js
    
    Co-Authored-By: kbuci <krishenb@uci.edu>
    
    * Update app/server/services/sendgrid_email.js
    
    Co-Authored-By: kbuci <krishenb@uci.edu>
    
    * Update app/server/services/sendgrid_email.js
    
    Co-Authored-By: kbuci <krishenb@uci.edu>
    
    * Update scripts/testSendGridTemplates.js
    
    Co-Authored-By: kbuci <krishenb@uci.edu>
    
    * Update scripts/testSendGridTemplates.js
    
    Co-Authored-By: kbuci <krishenb@uci.edu>
    
    * Update scripts/testSendGridTemplates.js
    
    Co-Authored-By: kbuci <krishenb@uci.edu>
    
    * Update .env.config
    
    Co-Authored-By: kbuci <krishenb@uci.edu>

---
+++ app/server/services/sendgrid_email.js
@@ -0,0 +1,115 @@
+var moment = require('moment');
+var ROOT_URL = process.env.ROOT_URL;
+
+var SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+var HACKATHON_NAME = process.env.HACKATHON_NAME;
+var EMAIL_ADDRESS = process.env.EMAIL_ADDRESS;
+var TWITTER_HANDLE = process.env.TWITTER_HANDLE;
+var FACEBOOK_HANDLE = process.env.FACEBOOK_HANDLE;
+var ACCEPTANCE_EMAIL_TEMPLATE = process.env.ACCEPTANCE_EMAIL_TEMPLATE;
+var VERIFICATION_EMAIL_TEMPLATE = process.env.VERIFICATION_EMAIL_TEMPLATE;
+var PASSWORD_RESET_EMAIL_TEMPLATE = process.env.PASSWORD_RESET_EMAIL_TEMPLATE;
+var PASSWORD_CHANGED_EMAIL_TEMPLATE = process.env.PASSWORD_CHANGED_EMAIL_TEMPLATE;
+var WAIVER_EMAIL_TEMPLATE = process.env.WAIVER_EMAIL_TEMPLATE;
+var NODE_ENV = process.env.NODE_ENV;
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+var emailService = {};
+
+/**
+ * Send an email with a dynamic template using SendGrid email API
+ * @param  {string}           template      [ID of SendGrid Dynamic Template]        
+ * @param  {string|string[]}  email         [Email address(s) to send to]
+ * @param  {Object}           templateData  [Mapping of template dynamic fields to values]
+ * @param  {Function}         callback      [Callback to be executed on success or error]
+ */
+function sendOne(template, email, templateData, callback) {
+  var msg = {
+    to: email,
+    from: EMAIL_ADDRESS,
+    templateId: template,
+    dynamic_template_data: templateData
+  };
+  sgMail.send(msg, (err, info) => {
+    if (err) {
+      console.error('Error sending email through SendGrid: ' + err);
+    }
+    if (info) {
+      console.warn('Email with template ' + template + ' sent to ' + email + ' with response:\n');
+      if (info[0] && info[0].toJSON) {
+        console.warn(info[0].toJSON());
+      }
+      else {
+        console.error("Ill-formed SendGrid response log"); 
+      }
+    }
+    if (callback) {
+      callback(err, info);
+    }
+  });
+}
+
+/**
+ * Send a verification email to a user, with a verification token to enter.
+ * @param  {string|string[]}  email         [Email address(s) to send to]
+ * @param  {[type]}     token    [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+emailService.sendVerificationEmail = function (email, token, callback) {
+  var verifyUrlData = {
+    url: `${ROOT_URL}/verify/${token}`
+  };
+  sendOne(VERIFICATION_EMAIL_TEMPLATE, email, verifyUrlData, callback);
+};
+
+/**
+ * Send a password recovery email.
+ * @param  {string|string[]}  email         [Email address(s) to send to]
+ * @param  {[type]}   token    [description]
+ * @param  {Function} callback [description]
+ */
+emailService.sendPasswordResetEmail = function (email, token, callback) {
+  var resetUrlData = {
+    url: `${ROOT_URL}/reset/${token}`
+  }
+  sendOne(PASSWORD_RESET_EMAIL_TEMPLATE, email, resetUrlData, callback);
+};
+
+/**
+ * Send a password recovery email.
+ * @param  {string|string[]}  email         [Email address(s) to send to]
+ * @param  {Function} callback [description]
+ */
+emailService.sendPasswordChangedEmail = function (email, callback) {
+  sendOne(PASSWORD_CHANGED_EMAIL_TEMPLATE, email, {}, callback);
+};
+
+/**
+ * Send the acceptance email to the participant.
+ * @param  {string|string[]}  email         [Email address(s) to send to]
+ * @param  {[type]}   confirmBy [description]
+ * @param  {Function} callback  [description]
+ */
+emailService.sendAcceptanceEmail = function (email, confirmBy, callback) {
+  confirmDate = { 
+    date: moment(confirmBy).format('MMMM D, YYYY h:mm A') 
+  };
+  sendOne(ACCEPTANCE_EMAIL_TEMPLATE, email, confirmDate, callback);
+};
+
+/**
+ * Send the waiver email to the participant.
+ * @param  {string|string[]}  email         [Email address(s) to send to]
+ * @param  {Function} callback  [description]
+ */
+emailService.sendWaiverEmail = function (email, callback) {
+  var userEmailData = {
+    email
+  };
+  sendOne(WAIVER_EMAIL_TEMPLATE, email, userEmailData, callback);
+};
+
+module.exports = emailService;
---
+++ scripts/testSendGridTemplates.js
@@ -0,0 +1,22 @@
+// Script to test every (dynamic) SendGrid email template with SendGrid email API
+// The env email address should recieve every template type defined in ENV (sent from itself)
+require('dotenv').load();
+
+var Mailer = require('../app/server/services/sendgrid_email');
+var email = process.env.EMAIL_ADDRESS;
+var templates = ['verification','passwordChanged','passwordReset','accepted', 'waiver'];
+var mailCheck = function (templateIndex) {
+    var ret = function (err, data) {
+        if (!err && data) {
+            console.warn('Email for template ' + templates[templateIndex] + ' sent');
+        }
+    };
+    return ret;
+};
+
+Mailer.sendVerificationEmail(email, 'testToken', mailCheck(0));
+Mailer.sendPasswordResetEmail(email, 'testToken', mailCheck(1));
+Mailer.sendPasswordChangedEmail(email, mailCheck(2));
+Mailer.sendAcceptanceEmail(email, 0, mailCheck(3));
+Mailer.sendWaiverEmail(email, mailCheck(4));
+
