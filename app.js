7f120b2b1d5dc21afa39f2f53a1990cd6c973c70
Jack Cook <hello@jackcook.nyc>
2018-11-06 00:08:18 -0500

    Reverted jsonwebtoken to fix minor auth issues

--- app.js
+++ app.js
@@ -46,4 +46,3 @@ require('./app/server/routes')(app);
 // listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
-
