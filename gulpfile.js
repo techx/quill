13a6f31aa0a8f0ade8701abc7fddd0b7f9069f79
Jack Cook <hello@jackcook.nyc>
2018-11-08 22:10:59 -0500

    Fixed semantic-ui dependency

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 
-var environment = process.env.NODE_ENV;
+var environment = process.env.NODE_ENV || "dev";
 
 var nodemon = require('gulp-nodemon');
 
@@ -27,32 +27,21 @@ gulp.task('default', function(){
 });
 
 gulp.task('js', function () {
-  if (environment !== 'dev'){
-    // Minify for non-development
-    gulp.src(['app/client/src/**/*.js', 'app/client/views/**/*.js'])
-      .pipe(sourcemaps.init())
-        .pipe(concat('app.js'))
-        .pipe(ngAnnotate())
-        .on('error', swallowError)
-        .pipe(uglify())
-      .pipe(gulp.dest('app/client/build'));
-  } else {
-    var b = browserify({
-        entries: 'app/client/src/app.js',
-        debug: true,
-        transform: [browserifyNgAnnotate]
-    });
+  var b = browserify({
+    entries: 'app/client/src/app.js',
+    debug: environment === "dev",
+    transform: [browserifyNgAnnotate]
+  });
 
-    // transform streaming contents into buffer contents (because gulp-sourcemaps does not support streaming contents)
-    b.bundle()
-      .pipe(source('app.js'))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(ngAnnotate())
-      .on('error', swallowError)
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('app/client/build'));
-  }
+  // transform streaming contents into buffer contents (because gulp-sourcemaps does not support streaming contents)
+  b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(ngAnnotate())
+    .on('error', swallowError)
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/client/build'));
 });
 
 gulp.task('semantic', function() {
