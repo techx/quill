bda62f3a3d1f7fa76370b1be97fc930a0a889064
Yasser El Habib DRIF <gy_drif@esi.dz>
2018-12-10 02:54:40 +0100

    test update

--- app/server/services/stats.js
+++ app/server/services/stats.js
@@ -104,7 +104,7 @@ function calculateStats(){
         newStats.confirmed += user.status.confirmed ? 1 : 0;
 
         // Count confirmed that are mit
-        newStats.confirmedMit += user.status.confirmed && email === "mit.edu" ? 1 : 0;
+        newStats.confirmedMit += user.status.confirmed && email === "mit.ed" ? 1 : 0;
 
         newStats.confirmedFemale += user.status.confirmed && user.profile.gender == "F" ? 1 : 0;
         newStats.confirmedMale += user.status.confirmed && user.profile.gender == "M" ? 1 : 0;
