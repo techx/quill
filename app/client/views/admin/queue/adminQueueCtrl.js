89ba139a44a80368bf45b4d566fbf6f5a63c86ab
Ryan Luu <ryanluu12345@gmail.com>
2018-12-23 22:15:06 -0800

    Added queue controller and html files to display the data from the queue. Added a new route for the routes admin page. Added the option to select the queue page in the admin.html page

---
+++ app/client/views/admin/queue/adminQueueCtrl.js
@@ -0,0 +1,53 @@
+angular.module('reg')
+  .controller('AdminQueueCtrl',[
+    '$scope',
+    '$state',
+    '$stateParams',
+    'UserService',
+    '$http',
+    '$window',
+    function($scope, $state, $stateParams, UserService, $http, $window){
+      $scope.pages = [];
+      $scope.users = [];
+
+      // Semantic-UI moves modal content into a dimmer at the top level.
+      // While this is usually nice, it means that with our routing will generate
+      // multiple modals if you change state. Kill the top level dimmer node on initial load
+      // to prevent this.
+      $('.ui.dimmer').remove();
+
+      UserService
+        .getAll()
+        .success(function(data){
+          $scope.users = data;
+        });
+
+      $scope.$watch('queryText', function(queryText){
+        UserService
+          .getPage($stateParams.page, $stateParams.size, queryText)
+          .success(function(data){
+
+          });
+      });
+
+      $scope.removeFromQueue = function($event, user, index) {
+        $event.stopPropagation();
+
+        swal({
+          title: "Whoa, wait a minute!",
+          text: "You are about to remove " + user.profile.name + " from the queue!",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#DD6B55",
+          confirmButtonText: "Yes, remove them.",
+          closeOnConfirm: false
+          }, function(){
+              UserService
+              .checkIn(user._id) //TODO CHANGE FUNCTION TO REMOVE FROM QUEUE
+              .success(function(user){
+                $scope.users[index] = user;
+                swal("Removed", user.profile.name + ' has been removed from the queue.', "success");
+              });
+          });
+      }
+    }]);
