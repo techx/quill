1630fe08ac682c56386e6dbed5bd7533facb9866
Freniel Zabala <frenielzabala@dhcp-v010-040.mobile.uci.edu>
2019-01-07 19:11:16 -0800

    code of conduct and name change

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -22,5 +22,5 @@ angular.module('reg')
         ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Your confirmation deadline of [CONFIRM_DEADLINE] has passed.',
         ADMITTED_AND_CANNOT_CONFIRM: 'Although you were accepted, you did not complete your confirmation in time.\nUnfortunately, this means that you will not be able to attend the event, as we must begin to accept other applicants on the waitlist.\nWe hope to see you again next year!',
         CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE]',
-        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to HackMIT 2015! :(\nMaybe next year! We hope you see you again soon.',
+        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to HackUCI 2019! :(\nMaybe next year! We hope you see you again soon.',
     })
