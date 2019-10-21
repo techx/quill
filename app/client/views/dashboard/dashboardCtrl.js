daf081c8c850509d4a0194985f5c05a0dd58f40d
mhendrick98 <mhendric@bu.edu>
2019-10-21 15:58:27 -0400

    it's hot fix summer

--- app/client/views/dashboard/dashboardCtrl.js
+++ app/client/views/dashboard/dashboardCtrl.js
@@ -1,108 +1,141 @@
-angular.module('reg')
-  .controller('DashboardCtrl', [
-    '$rootScope',
-    '$scope',
-    '$sce',
-    'currentUser',
-    'settings',
-    'Utils',
-    'AuthService',
-    'UserService',
-    'EVENT_INFO',
-    'DASHBOARD',
-    function($rootScope, $scope, $sce, currentUser, settings, Utils, AuthService, UserService, EVENT_INFO, DASHBOARD){
-      var Settings = settings.data;
-      var user = currentUser.data;
-      $scope.user = user;
-      $scope.timeClose = Utils.formatTime(Settings.timeClose);
-      $scope.timeConfirm = Utils.formatTime(Settings.timeConfirm);
+angular.module("reg").controller("DashboardCtrl", [
+  "$rootScope",
+  "$scope",
+  "$sce",
+  "currentUser",
+  "settings",
+  "Utils",
+  "AuthService",
+  "UserService",
+  "EVENT_INFO",
+  "DASHBOARD",
+  function(
+    $rootScope,
+    $scope,
+    $sce,
+    currentUser,
+    settings,
+    Utils,
+    AuthService,
+    UserService,
+    EVENT_INFO,
+    DASHBOARD
+  ) {
+    var Settings = settings.data;
+    var user = currentUser.data;
+    $scope.user = user;
+    $scope.timeClose = Utils.formatTime(Settings.timeClose);
+    $scope.timeConfirm = Utils.formatTime(Settings.timeConfirm);
 
-      $scope.DASHBOARD = DASHBOARD;
+    $scope.DASHBOARD = DASHBOARD;
 
-      for (var msg in $scope.DASHBOARD) {
-        if ($scope.DASHBOARD[msg].includes('[APP_DEADLINE]')) {
-          $scope.DASHBOARD[msg] = $scope.DASHBOARD[msg].replace('[APP_DEADLINE]', Utils.formatTime(Settings.timeClose));
-        }
-        if ($scope.DASHBOARD[msg].includes('[CONFIRM_DEADLINE]')) {
-          $scope.DASHBOARD[msg] = $scope.DASHBOARD[msg].replace('[CONFIRM_DEADLINE]', Utils.formatTime(user.status.confirmBy));
-        }
+    for (var msg in $scope.DASHBOARD) {
+      if ($scope.DASHBOARD[msg].includes("[APP_DEADLINE]")) {
+        $scope.DASHBOARD[msg] = $scope.DASHBOARD[msg].replace(
+          "[APP_DEADLINE]",
+          Utils.formatTime(Settings.timeClose)
+        );
       }
+      if ($scope.DASHBOARD[msg].includes("[CONFIRM_DEADLINE]")) {
+        $scope.DASHBOARD[msg] = $scope.DASHBOARD[msg].replace(
+          "[CONFIRM_DEADLINE]",
+          Utils.formatTime(user.status.confirmBy)
+        );
+      }
+    }
 
-      // Is registration open?
-      var regIsOpen = $scope.regIsOpen = Utils.isRegOpen(Settings);
+    // Is registration open?
+    var regIsOpen = ($scope.regIsOpen = Utils.isRegOpen(Settings));
 
-      // Is it past the user's confirmation time?
-      var pastConfirmation = $scope.pastConfirmation = Utils.isAfter(user.status.confirmBy);
+    // Is it past the user's confirmation time?
+    var pastConfirmation = ($scope.pastConfirmation = Utils.isAfter(
+      user.status.confirmBy
+    ));
 
-      $scope.dashState = function(status){
-        var user = $scope.user;
-        switch (status) {
-          case 'unverified':
-            return !user.verified;
-          case 'openAndIncomplete':
-            return regIsOpen && user.verified && !user.status.completedProfile;
-          case 'openAndSubmitted':
-            return regIsOpen && user.status.completedProfile && !user.status.admitted;
-          case 'closedAndIncomplete':
-            return !regIsOpen && !user.status.completedProfile && !user.status.admitted;
-          case 'closedAndSubmitted': // Waitlisted State
-            return !regIsOpen && user.status.completedProfile && !user.status.admitted;
-          case 'admittedAndCanConfirm':
-            return !pastConfirmation &&
-              user.status.admitted &&
-              !user.status.confirmed &&
-              !user.status.declined;
-          case 'admittedAndCannotConfirm':
-            return pastConfirmation &&
-              user.status.admitted &&
-              !user.status.confirmed &&
-              !user.status.declined;
-          case 'confirmed':
-            return user.status.admitted && user.status.confirmed && !user.status.declined;
-          case 'declined':
-            return user.status.declined;
-        }
-        return false;
-      };
+    $scope.dashState = function(status) {
+      var user = $scope.user;
+      switch (status) {
+        case "unverified":
+          return !user.verified;
+        case "openAndIncomplete":
+          return regIsOpen && user.verified && !user.status.completedProfile;
+        case "openAndSubmitted":
+          return (
+            regIsOpen && user.status.completedProfile && !user.status.admitted
+          );
+        case "closedAndIncomplete":
+          return (
+            !regIsOpen && !user.status.completedProfile && !user.status.admitted
+          );
+        case "closedAndSubmitted":
+          return !regIsOpen && user.status.completedProfile;
+        case "admittedAndCanConfirm":
+          return (
+            !pastConfirmation &&
+            user.status.admitted &&
+            !user.status.confirmed &&
+            !user.status.declined
+          );
+        case "admittedAndCannotConfirm":
+          return (
+            pastConfirmation &&
+            user.status.admitted &&
+            !user.status.confirmed &&
+            !user.status.declined
+          );
+        case "confirmed":
+          return (
+            user.status.admitted &&
+            user.status.confirmed &&
+            !user.status.declined
+          );
+        case "declined":
+          return user.status.declined;
+      }
+      return false;
+    };
 
-      $scope.showWaitlist = !regIsOpen && user.status.completedProfile && !user.status.admitted;
+    $scope.showWaitlist = !regIsOpen && !user.status.completedProfile;
 
-      $scope.resendEmail = function(){
-        AuthService
-          .resendVerificationEmail()
-          .then(function(){
-            sweetAlert('Your email has been sent.');
-          });
-      };
+    $scope.resendEmail = function() {
+      AuthService.resendVerificationEmail().then(function() {
+        sweetAlert("Your email has been sent.");
+      });
+    };
 
+    // -----------------------------------------------------
+    // Text!
+    // -----------------------------------------------------
+    var converter = new showdown.Converter();
+    $scope.acceptanceText = $sce.trustAsHtml(
+      converter.makeHtml(Settings.acceptanceText)
+    );
+    $scope.confirmationText = $sce.trustAsHtml(
+      converter.makeHtml(Settings.confirmationText)
+    );
+    $scope.waitlistText = $sce.trustAsHtml(
+      converter.makeHtml(Settings.waitlistText)
+    );
 
-      // -----------------------------------------------------
-      // Text!
-      // -----------------------------------------------------
-      var converter = new showdown.Converter();
-      $scope.acceptanceText = $sce.trustAsHtml(converter.makeHtml(Settings.acceptanceText));
-      $scope.confirmationText = $sce.trustAsHtml(converter.makeHtml(Settings.confirmationText));
-      $scope.waitlistText = $sce.trustAsHtml(converter.makeHtml(Settings.waitlistText));
-
-      $scope.declineAdmission = function(){
-
-        swal({
+    $scope.declineAdmission = function() {
+      swal(
+        {
           title: "Whoa!",
-          text: "Are you sure you would like to decline your admission? \n\n You can't go back!",
+          text:
+            "Are you sure you would like to decline your admission? \n\n You can't go back!",
           type: "warning",
           showCancelButton: true,
           confirmButtonColor: "#DD6B55",
           confirmButtonText: "Yes, I can't make it.",
           closeOnConfirm: true
-          }, function(){
-
-            UserService
-              .declineAdmission(user._id)
-              .success(function(user){
-                $rootScope.currentUser = user;
-                $scope.user = user;
-              });
-        });
-      };
-
-    }]);
+        },
+        function() {
+          UserService.declineAdmission(user._id).success(function(user) {
+            $rootScope.currentUser = user;
+            $scope.user = user;
+          });
+        }
+      );
+    };
+  }
+]);
