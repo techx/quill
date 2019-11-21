4f4e448fc4c7aa5745ad8b9d4ffe82d39d630ef8
Kyle Rubenok <kyle@rubenok.ca>
2019-11-21 18:21:54 -0500

    A better test case

---
+++ test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const app = require("./app.js");
+
+
+describe("Get Login", () => {
+    it("Should Get the login Page   ", async () => {
+        const res = await request(app)
+            .get("/login")
+            .end()
+        expect(res.statusCode).toEqual(200);
+    });
+});
