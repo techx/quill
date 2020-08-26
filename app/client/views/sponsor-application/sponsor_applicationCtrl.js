91e18c251d17db8342e7a33eb3e616120d03319c
Ryan Chhong <ryanchhong@gmail.com>
2020-08-25 20:58:14 -0500

    finished back-end updates to support new form

--- app/client/views/sponsor-application/sponsor_applicationCtrl.js
+++ app/client/views/sponsor-application/sponsor_applicationCtrl.js
@@ -20,85 +20,80 @@ angular.module('reg')
       $scope.SPONSORSHIP_COST = SPONSORSHIP_COST;
       $scope.isTitle = $scope.user.sponsorFields.tier === "title";
       $scope.isGiga = $scope.user.sponsorFields.tier === "giga";
-      $scope.estimatedCost = 0;
-
-
 
       // Cost for timed events, i.e. opening/closing remarks
-      var timedCost = 0;
+      var addOnsCost = 0;
       // Flat costs, i.e. workshops/tiers
-      var flatCosts = 0;
+      var tierCost = 0;
 
-      var totalCost = timedCost + flatCosts;
+      var totalCost = addOnsCost + tierCost;
 
       // Watch for changes in tier/workshop/times, as they effect the cost
       $scope.$watchGroup([
         'user.sponsorFields.tier',
-        'user.sponsorFields.workshop',
       ],
       function(newValue, oldValue, scope) {
         console.log($scope.user);
         var isTitle = $scope.user.sponsorFields.tier === "title";
         var isGiga = $scope.user.sponsorFields.tier === "giga";
 
-        flatCosts = 0;
-
-        var newTier = newValue[0], newWorkshopValue = newValue[1];
+        var newTier = newValue[0];
         // Compute tier cost
-        var tierCost = newTier ? $scope.SPONSORSHIP_COST[newTier.toUpperCase()] : 0;
-        flatCosts += tierCost;
-
-        // If not title sponsor, charge for workshops
-        if(!isTitle) {
-          if(newWorkshopValue) {
-            flatCosts += SPONSORSHIP_COST['WORKSHOP'];
-          }
-        }
+        tierCost = newTier ? $scope.SPONSORSHIP_COST[newTier.toUpperCase()] : 0;
 
         if(isGiga) {
           $scope.user.sponsorFields.openingStatementTime = "60";
-          $scope.user.sponsorFields.closingStatementTime = "  ";
+          $scope.user.sponsorFields.closingStatementTime = "0";
         }
         else if(isTitle) {
           $scope.user.sponsorFields.openingStatementTime = "120";
           $scope.user.sponsorFields.closingStatementTime = "60";
         }
         else {
-          $scope.user.sponsorFields.openingStatementTime = "";
-          $scope.user.sponsorFields.closingStatementTime = "";
+          $scope.user.sponsorFields.openingStatementTime = "0";
+          $scope.user.sponsorFields.closingStatementTime = "0";
         }
 
-        totalCost = timedCost + flatCosts;
+        totalCost = tierCost + addOnsCost;
         $scope.isTitle = isTitle;
         $scope.isGiga = isGiga;
-        $scope.estimatedCost = totalCost;
+        $scope.user.sponsorFields.estimatedCost = totalCost;
       });
 
-      // Monitor opening/closing remarks
+      // Monitor add-ons
       $scope.$watchGroup([
         'user.sponsorFields.openingStatementTime',
         'user.sponsorFields.closingStatementTime',
+        'user.sponsorFields.workshop',
       ],
       function(newValue, oldValue, scope) {
         var isTitle = $scope.user.sponsorFields.tier === "title";
         var isGiga = $scope.user.sponsorFields.tier === "giga";
 
-        timedCosts = 0;
+        addOnsCost = 0;
 
-        var newOpeningTime = newValue[0], newClosingTime = newValue[1];
+        var newOpeningTime = newValue[0], newClosingTime = newValue[1],  newWorkshopValue = newValue[2];
 
+        // If not title sponsor, charge for workshops
+        if(!isTitle) {
+          if(newWorkshopValue) {
+            addOnsCost += SPONSORSHIP_COST['WORKSHOP'];
+          }
+        }
+
+        // Compute rate for opening/closing remarks
         if(isGiga) {
-          timedCost = (Number(newClosingTime) / SPONSORSHIP_COST['TIMED_RATE']) * SPONSORSHIP_COST['TIMED_COST'];
+          addOnsCost += (Number(newClosingTime) / SPONSORSHIP_COST['TIMED_RATE']) * SPONSORSHIP_COST['TIMED_COST'];
         }
         else if(isTitle) {
           // Do nothing, costs already factored in
         }
         else {
-          timedCost = ((Number(newOpeningTime) + Number(newClosingTime)) / SPONSORSHIP_COST['TIMED_RATE']) * SPONSORSHIP_COST['TIMED_COST'];
+          addOnsCost += ((Number(newOpeningTime) + Number(newClosingTime)) / SPONSORSHIP_COST['TIMED_RATE']) * SPONSORSHIP_COST['TIMED_COST'];
         }
 
-        totalCost = timedCost + flatCosts;
-        $scope.estimatedCost = totalCost;
+        totalCost = tierCost + addOnsCost;
+        $scope.user.sponsorFields.estimatedCost = totalCost;
       });
 
       _setupForm();
@@ -118,40 +113,62 @@ angular.module('reg')
       }
 
       function _setupForm() {
-        // Semantic-UI form validation
-        // $('.ui.form').form({
-        //   inline: true,
-        //   fields: {
-        //     companyName: {
-        //       identifier: 'companyName',
-        //       rules: [
-        //         {
-        //           type: 'empty',
-        //           prompt: 'Please enter a company name.'
-        //         }
-        //       ]
-        //     },
-        //     PlegeAmount: {
-        //       identifier: 'PlegeAmount',
-        //       rules: [
-        //         {
-        //           type: 'empty',
-        //           prompt: 'Please enter an amount.'
-        //         }
-        //       ]
-        //     },
-        //     RelevantLinks: {
-        //       identifier: 'RelevantLinks',
-        //       rules: [
-        //         {
-        //           type: 'empty',
-        //           prompt: 'Please list some relevant links for your hackathon idea.'
-        //         }
-        //       ]
-        //     }
-        //   },
-        //   on: 'blur'
-        // });
+        //Semantic-UI form validation
+        $('.ui.form').form({
+          inline: true,
+          fields: {
+            companyName: {
+              identifier: 'companyName',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter a company name.'
+                }
+              ]
+            },
+            representativeEmail: {
+              identifier: 'representativeEmail',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter a email.'
+                },
+                {
+                  type: 'email',
+                  prompt: 'Please enter a valid email.'
+                }
+              ]
+            },
+            representativeFirstName: {
+              identifier: 'representativeFirstName',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter a first name.'
+                }
+              ]
+            },
+            representativeLastName: {
+              identifier: 'representativeLastName',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter a first name.'
+                }
+              ]
+            },
+            tier: {
+              identifier: 'tier',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please select a valid tier.'
+                }
+              ]
+            },
+          },
+          on: 'blur'
+        });
       }
 
       $scope.submitForm = function () {
