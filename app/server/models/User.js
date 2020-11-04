daf833d65ffd7997173d141007c4a7a94f5a9f47
David Richey <darichey1@gmail.com>
2020-11-03 22:51:00 -0600

    Initial changes for HackUTD

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -28,7 +28,7 @@ var profile = {
   graduationYear: {
     type: String,
     enum: {
-      values: '2016 2017 2018 2019'.split(' '),
+      values: '2021 2022 2023 2024'.split(' '),
     }
   },
 
@@ -333,7 +333,7 @@ schema.statics.validateProfile = function(profile, cb){
     profile.name.length > 0 &&
     profile.adult &&
     profile.school.length > 0 &&
-    ['2016', '2017', '2018', '2019'].indexOf(profile.graduationYear) > -1 &&
+    ['2021', '2022', '2023', '2024'].indexOf(profile.graduationYear) > -1 &&
     ['M', 'F', 'O', 'N'].indexOf(profile.gender) > -1
     ));
 };
