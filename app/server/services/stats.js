c09054c0cfe7acf76bd35b0437dd0cee855a7bdf
lasri <lasridor@gmail.com>
2021-08-27 02:23:31 +0300

    stats update with mentors data

--- app/server/services/stats.js
+++ app/server/services/stats.js
@@ -10,6 +10,8 @@ function calculateStats(){
     lastUpdated: 0,
 
     total: 0,
+    totalHackers: 0,
+    totalMentors: 0,
     demo: {
       gender: {
         M: 0,
@@ -18,28 +20,60 @@ function calculateStats(){
         N: 0
       },
       schools: {},
-      year: {
-        '2016': 0,
-        '2017': 0,
-        '2018': 0,
-        '2019': 0,
-      }
+      companys: {},
+      years: {}
     },
-
     teams: {},
-    verified: 0,
-    submitted: 0,
-    admitted: 0,
-    confirmed: 0,
-    confirmedMit: 0,
-    declined: 0,
+
+    verifiedHackers: 0,
+    submittedHackers: 0,
+    admittedHackers: 0,
+    confirmedHackers: 0,
+    declinedHackers: 0,
+    verifiedMentors: 0,
+    submittedMentors: 0,
+    admittedMentors: 0,
+    confirmedMentors: 0,
+    declinedMentors: 0,
 
     confirmedFemale: 0,
     confirmedMale: 0,
     confirmedOther: 0,
     confirmedNone: 0,
 
-    shirtSizes: {
+    shirtSizesHackers: {
+      'XS': 0,
+      'S': 0,
+      'M': 0,
+      'L': 0,
+      'XL': 0,
+      'XXL': 0,
+      'WXS': 0,
+      'WS': 0,
+      'WM': 0,
+      'WL': 0,
+      'WXL': 0,
+      'WXXL': 0,
+      'None': 0
+    },
+
+    shirtSizesHackers: {
+      'XS': 0,
+      'S': 0,
+      'M': 0,
+      'L': 0,
+      'XL': 0,
+      'XXL': 0,
+      'WXS': 0,
+      'WS': 0,
+      'WM': 0,
+      'WL': 0,
+      'WXL': 0,
+      'WXXL': 0,
+      'None': 0
+    },
+
+    shirtSizesMentors: {
       'XS': 0,
       'S': 0,
       'M': 0,
@@ -57,21 +91,10 @@ function calculateStats(){
 
     dietaryRestrictions: {},
 
-    hostNeededFri: 0,
-    hostNeededSat: 0,
-    hostNeededUnique: 0,
-
-    hostNeededFemale: 0,
-    hostNeededMale: 0,
-    hostNeededOther: 0,
-    hostNeededNone: 0,
-
-    reimbursementTotal: 0,
-    reimbursementMissing: 0,
-
     wantsHardware: 0,
 
-    checkedIn: 0
+    checkedInHackers: 0,
+    checkedInMentors: 0
   };
 
   User
@@ -91,84 +114,13 @@ function calculateStats(){
         // Add to the gender
         newStats.demo.gender[user.profile.gender] += 1;
 
-        // Count verified
-        newStats.verified += user.verified ? 1 : 0;
-
-        // Count submitted
-        newStats.submitted += user.status.completedProfile ? 1 : 0;
-
-        // Count accepted
-        newStats.admitted += user.status.admitted ? 1 : 0;
-
-        // Count confirmed
-        newStats.confirmed += user.status.confirmed ? 1 : 0;
-
-        // Count confirmed that are mit
-        newStats.confirmedMit += user.status.confirmed && email === "mit.edu" ? 1 : 0;
-
-        newStats.confirmedFemale += user.status.confirmed && user.profile.gender == "F" ? 1 : 0;
-        newStats.confirmedMale += user.status.confirmed && user.profile.gender == "M" ? 1 : 0;
-        newStats.confirmedOther += user.status.confirmed && user.profile.gender == "O" ? 1 : 0;
-        newStats.confirmedNone += user.status.confirmed && user.profile.gender == "N" ? 1 : 0;
-
-        // Count declined
-        newStats.declined += user.status.declined ? 1 : 0;
-
-        // Count the number of people who need reimbursements
-        newStats.reimbursementTotal += user.confirmation.needsReimbursement ? 1 : 0;
-
-        // Count the number of people who still need to be reimbursed
-        newStats.reimbursementMissing += user.confirmation.needsReimbursement &&
-          !user.status.reimbursementGiven ? 1 : 0;
+        newStats.confirmedFemale += (user.status.confirmedHackers || user.status.confirmedMentors) && user.profile.gender == "F" ? 1 : 0;
+        newStats.confirmedMale += (user.status.confirmedHackers || user.status.confirmedMentors) && user.profile.gender == "M" ? 1 : 0;
+        newStats.confirmedOther += (user.status.confirmedHackers || user.status.confirmedMentors) && user.profile.gender == "O" ? 1 : 0;
+        newStats.confirmedNone += (user.status.confirmedHackers || user.status.confirmedMentors) && user.profile.gender == "N" ? 1 : 0;
 
         // Count the number of people who want hardware
         newStats.wantsHardware += user.confirmation.wantsHardware ? 1 : 0;
-
-        // Count schools
-        if (!newStats.demo.schools[email]){
-          newStats.demo.schools[email] = {
-            submitted: 0,
-            admitted: 0,
-            confirmed: 0,
-            declined: 0,
-          };
-        }
-        newStats.demo.schools[email].submitted += user.status.completedProfile ? 1 : 0;
-        newStats.demo.schools[email].admitted += user.status.admitted ? 1 : 0;
-        newStats.demo.schools[email].confirmed += user.status.confirmed ? 1 : 0;
-        newStats.demo.schools[email].declined += user.status.declined ? 1 : 0;
-
-        // Count graduation years
-        if (user.profile.graduationYear){
-          newStats.demo.year[user.profile.graduationYear] += 1;
-        }
-
-        // Grab the team name if there is one
-        // if (user.teamCode && user.teamCode.length > 0){
-        //   if (!newStats.teams[user.teamCode]){
-        //     newStats.teams[user.teamCode] = [];
-        //   }
-        //   newStats.teams[user.teamCode].push(user.profile.name);
-        // }
-
-        // Count shirt sizes
-        if (user.confirmation.shirtSize in newStats.shirtSizes){
-          newStats.shirtSizes[user.confirmation.shirtSize] += 1;
-        }
-
-        // Host needed counts
-        newStats.hostNeededFri += user.confirmation.hostNeededFri ? 1 : 0;
-        newStats.hostNeededSat += user.confirmation.hostNeededSat ? 1 : 0;
-        newStats.hostNeededUnique += user.confirmation.hostNeededFri || user.confirmation.hostNeededSat ? 1 : 0;
-
-        newStats.hostNeededFemale
-          += (user.confirmation.hostNeededFri || user.confirmation.hostNeededSat) && user.profile.gender == "F" ? 1 : 0;
-        newStats.hostNeededMale
-          += (user.confirmation.hostNeededFri || user.confirmation.hostNeededSat) && user.profile.gender == "M" ? 1 : 0;
-        newStats.hostNeededOther
-          += (user.confirmation.hostNeededFri || user.confirmation.hostNeededSat) && user.profile.gender == "O" ? 1 : 0;
-        newStats.hostNeededNone
-          += (user.confirmation.hostNeededFri || user.confirmation.hostNeededSat) && user.profile.gender == "N" ? 1 : 0;
 
         // Dietary restrictions
         if (user.confirmation.dietaryRestrictions){
@@ -180,8 +132,108 @@ function calculateStats(){
           });
         }
 
-        // Count checked in
-        newStats.checkedIn += user.status.checkedIn ? 1 : 0;
+        if(user.mentor === true){
+          newStats.totalMentors += 1;
+
+          // Count verified
+          newStats.verifiedMentors += user.verified ? 1 : 0;
+
+          // Count submitted
+          newStats.submittedMentors += user.status.completedProfile ? 1 : 0;
+
+          // Count accepted
+          newStats.admittedMentors += user.status.admitted ? 1 : 0;
+
+          // Count confirmed
+          newStats.confirmedMentors += user.status.confirmed ? 1 : 0;
+
+          // Count declined
+          newStats.declinedMentors += user.status.declinedMentors ? 1 : 0;
+
+          // Count companys
+          if (!newStats.demo.companys[email]){
+            newStats.demo.companys[email] = {
+            submitted: 0,
+            admitted: 0,
+            confirmed: 0,
+            declined: 0,
+            };
+          }
+          newStats.demo.companys[email].submitted += user.status.completedProfile ? 1 : 0;
+          newStats.demo.companys[email].admitted += user.status.admitted ? 1 : 0;
+          newStats.demo.companys[email].confirmed += user.status.confirmed ? 1 : 0;
+          newStats.demo.companys[email].declined += user.status.declined ? 1 : 0;
+
+          newStats.checkedInMentors += user.status.checkedIn ? 1 : 0;
+
+          // Count shirt sizes
+          if (user.confirmation.shirtSize in newStats.shirtSizesMentors){
+            newStats.shirtSizesMentors[user.confirmation.shirtSize] += 1;
+          }
+        }
+        else{
+          newStats.totalHackers += 1;
+
+          // Count verified
+          newStats.verifiedHackers += user.verified ? 1 : 0;
+
+          // Count submitted
+          newStats.submittedHackers += user.status.completedProfile ? 1 : 0;
+
+          // Count accepted
+          newStats.admittedHackers += user.status.admitted ? 1 : 0;
+
+          // Count confirmed
+          newStats.confirmedHackers += user.status.confirmed ? 1 : 0;
+
+          // Count declined
+          newStats.declinedHackers += user.status.declinedHackers ? 1 : 0;
+
+
+          // Count schools
+          if (!newStats.demo.schools[email]){
+            newStats.demo.schools[email] = {
+              submitted: 0,
+              admitted: 0,
+              confirmed: 0,
+              declined: 0,
+            };
+          }
+          newStats.demo.schools[email].submitted += user.status.completedProfile ? 1 : 0;
+          newStats.demo.schools[email].admitted += user.status.admitted ? 1 : 0;
+          newStats.demo.schools[email].confirmed += user.status.confirmed ? 1 : 0;
+          newStats.demo.schools[email].declined += user.status.declined ? 1 : 0;
+
+          // Count years
+          if (!newStats.demo.years[user.profile.graduationYear]){
+              newStats.demo.years[user.profile.graduationYear] = {
+              count: 0,
+            };
+          }
+          newStats.demo.years[user.profile.graduationYear].count += newStats.demo.years[user.profile.graduationYear] ? 1 : 0;
+
+                  // Count checked in
+          newStats.checkedInHackers += user.status.checkedIn ? 1 : 0;
+
+          // Count shirt sizes
+          if (user.confirmation.shirtSize in newStats.shirtSizesHackers){
+            newStats.shirtSizesHackers[user.confirmation.shirtSize] += 1;
+          }
+        }
+
+
+        // Count graduation years
+        //if (user.profile.graduationYear){
+        //  newStats.demo.year[user.profile.graduationYear] += 1;
+        //}
+
+        // Grab the team name if there is one
+        // if (user.teamCode && user.teamCode.length > 0){
+        //   if (!newStats.teams[user.teamCode]){
+        //     newStats.teams[user.teamCode] = [];
+        //   }
+        //   newStats.teams[user.teamCode].push(user.profile.name);
+        // }
 
         callback(); // let async know we've finished
       }, function() {
@@ -207,6 +259,31 @@ function calculateStats(){
             });
           });
         newStats.demo.schools = schools;
+
+        // Transform schools into an array of objects
+        var companys = [];
+        _.keys(newStats.demo.companys)
+          .forEach(function(key){
+            companys.push({
+              email: key,
+              count: newStats.demo.companys[key].submitted,
+              stats: newStats.demo.companys[key]
+            });
+          });
+        newStats.demo.companys = companys;
+
+        // Transform years into an array of objects
+        var years = [];
+        _.keys(newStats.demo.years)
+        .forEach(function(key){
+            if(key != "undefined"){
+              years.push({
+                year: key,
+                count: newStats.demo.years[key],
+              });
+          }
+        });
+        newStats.demo.years = years;
 
         // Likewise, transform the teams into an array of objects
         // var teams = [];
