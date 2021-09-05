1011391474d510bab98ecfdb28c65a9f70c780de
Nadav <nadavsuliman@gmail.com>
2021-09-05 13:24:39 +0300

    Remove unecessary data in user profile page

--- app/client/views/profile/profileCtrl.js
+++ app/client/views/profile/profileCtrl.js
@@ -2,15 +2,8 @@ angular.module('reg')
   .controller('ProfileCtrl', [
     '$scope',
     'currentUser',
-    'settings',
     'Utils',
-    'UserService',
-    function($scope, currentUser, settings, Utils, UserService){
+    function($scope, currentUser, Utils){
       // Get the current user's most recent data.
-      var Settings = settings.data;
-
-      $scope.regIsOpen = Utils.isRegOpen(Settings);
-
       $scope.user = currentUser.data;
-
     }]);
